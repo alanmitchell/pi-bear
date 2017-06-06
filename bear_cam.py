8c079f0f5e5a94ba9bb5cedc35900dab278d906e
Alan Mitchell <tabb99@gmail.com>
2017-06-05 22:33:07 -0800

    Restored 30 fps frame rate.
    At 15 fps, video played back in fast motion.

--- bear_cam.py
+++ bear_cam.py
@@ -18,7 +18,7 @@ class Camera:
         # need to store in a temporary image file because file is constantly
         # changing while recording is occurring, and don't want to have a
         # partial file in the final media directory.
-        self.camera = picamera.PiCamera(framerate=15)
+        self.camera = picamera.PiCamera()
         self.camera.iso = 800
         self.camera.start_recording(self.tmp_img_file)
         self.is_on = True
